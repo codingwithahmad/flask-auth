3daedd6ecd3b273a1f20390a4404bc128ce61aa9
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-11-23 22:02:02 +0330

    initial commit

---
+++ auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from . import db
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+	return 'login'
+
+@auth.route('/signup')
+def signup():
+	return 'signup'
+
+@auth.route('/logout')
+def logout():
+	return 'logout'
