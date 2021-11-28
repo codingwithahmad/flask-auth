27aae805886cd3c79cd16accefd65926c9ba24f6
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-11-28 22:03:49 +0330

    step 08 is finished

--- project/__init__.py
+++ project/__init__.py
@@ -2,17 +2,14 @@ from decouple import config
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
-
 db = SQLAlchemy()
-
 
 def create_app():
 	app = Flask(__name__)
 
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	app.config['SECRET_KEY'] = config('SECRET_KEY')
-	app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.sqlite'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 
 	db.init_app(app)
